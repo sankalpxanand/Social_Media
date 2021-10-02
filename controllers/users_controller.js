685b552a4b59a192ad9c5cf09ff47ad2ea5e0ad6
Sankalp Anand <sankalpananddd29@gmail.com>
2021-10-02 21:23:30 +0530

    Render pages for Sign up and Sign in

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,16 @@ module.exports.profile = function(req, res){
         title: "Profile"
     });
 }
+//render the sign up page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up', {
+        title: "Social | Sign Up"
+    })
+}
+
+//render the sign in page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in', {
+        title: "Social | Sign In"
+    })
+}
