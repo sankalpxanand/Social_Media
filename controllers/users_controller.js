e83be138f7b48cfa0ac673906abb983c317435ac
Sankalp Anand <sankalpananddd29@gmail.com>
2021-10-03 17:14:09 +0530

    Sign in and create a sesssion for user

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -41,5 +41,27 @@ module.exports.create = function(req, res){
 }
 //sign in and create a session for the user
 module.exports.createSession = function(req, res){
-    //TODO later
+    //steps to aunthenticate
+
+    //find the user
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){
+            console.log('error in finding user in signing in');
+            return;
+        }
+        //handle user found
+        if(user){
+            //handle password which doesn't match
+            if(user.password!= req.body.password){
+                return res.redirect('back');
+            }
+            //handle session creation
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+        }
+        //handle user not found
+        else{
+            return res.redirect('back');
+        }
+    })
 }
