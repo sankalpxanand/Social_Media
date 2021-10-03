2151dff9722be1776e0865b5b3ad3f4818152f37
Sankalp Anand <sankalpananddd29@gmail.com>
2021-10-03 16:55:30 +0530

    Get the sign up data

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title: "Profile"
@@ -17,7 +19,25 @@ module.exports.signIn = function(req,res){
 }
 //get the sign up data
 module.exports.create = function(req, res){
-    //TODO later
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect ('back');
+    }
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){
+            console.log('error in finding user in signing up');
+        }
+        if(!user){
+            User.create(req.body, function(err, user){
+                if(err){
+                    console.log('error in creating user while signing up');
+                }
+                return res.redirect('/users/sign-in');
+            })
+        }
+        else{
+            return res.redirect ('back');
+        }
+    })
 }
 //sign in and create a session for the user
 module.exports.createSession = function(req, res){
