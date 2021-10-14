55d2d231fd9c517b582130463143d412993398b3
Sankalp Anand <sankalpananddd29@gmail.com>
2021-10-14 12:18:22 +0530

    Display the user of the post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -4,10 +4,18 @@ module.exports.home = function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
 
-    Post.find({}, function(err, posts){
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Social | Home",
+    //         posts: posts
+    //     });
+    // });
+
+    //populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
         return res.render('home', {
             title: "Social | Home",
             posts: posts
         });
-    });
+    })
 }
