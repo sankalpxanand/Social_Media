3675f0e6cd6f60b10cfac4f50584c27a3409becc
Sankalp Anand <sankalpananddd29@gmail.com>
2021-10-26 15:35:41 +0530

    Update a User's Profile

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const userController = require('../controllers/users_controller');
 const passport = require('passport');
 
 router.get('/profile/:id', passport.checkAuthentication, userController.profile);
+router.post('/update/:id', passport.checkAuthentication, userController.update);
 
 router.get('/sign-up', userController.signUp);
 router.get('/sign-in', userController.signIn);
