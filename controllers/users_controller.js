ed07e9654a7435ba6f317638309fdfa619fe7b7c
Sankalp Anand <sankalpananddd29@gmail.com>
2021-11-08 14:27:52 +0530

    Show the Avatar

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -11,16 +11,7 @@ module.exports.profile = function(req, res){
     })
 }
 module.exports.update = async function(req, res){
-    // if(req.user.id == req.params.id){
-    //     User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
-    //         return res.redirect('back');
-    //     })
-    // }
-    // else{
-    //     req.flash('error', 'Unauthorized');
-    //     return res.status(401).send('Unauthorized');
-    // }
-
+    
     if(req.user.id == req.params.id){
         try{
             let user = await User.findById(req.params.id);
