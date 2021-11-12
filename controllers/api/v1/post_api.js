a46278e2ecc926a707ad92aebc8c5bc4cfeaa51c
Sankalp Anand <sankalpananddd29@gmail.com>
2021-11-12 12:52:34 +0530

    Play with API: Render the list of post & Delete a post(without authentication and authority)

--- controllers/api/v1/post_api.js
+++ controllers/api/v1/post_api.js
@@ -1,6 +1,48 @@
-module.exports.index = function(req, res){
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
+
+module.exports.index = async function(req, res){
+
+    let posts = await Post.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+
     return res.json(200,{
         message: "List of Post",
-        posts: []
+        posts: posts
     })
 }
+
+module.exports.destroy = async function(req, res){
+    try{
+        let post = await Post.findById(req.params.id);
+        
+        //'.id' means coverting the object id into string 
+        // if(post.user == req.user.id){
+            post.remove();
+
+            await Comment.deleteMany({post: req.params.id});
+
+            return res.json(200, {
+                message: "Post and associtated comments deleted successfully!"
+            });
+        // }
+        // else{
+        //     req.flash('error', 'You cannot delete this post!');
+        //     return res.redirect('back');
+        // }
+    }
+    catch(err){
+        console.log('*********', err);
+        return res.json(500, {
+            message: "Internal Server Error"
+        });
+    }
+}
+
