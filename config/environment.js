3fe1a26d8a48d7e6f631e623ccf0adbbb6c59c37
Sankalp Anand <sankalpananddd29@gmail.com>
2021-12-08 16:01:51 +0530

    Environment Variables - Production Environment

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@ const development = {
     name: 'development',
     asset_path: '/assets',
     session_cookie_key: 'blahsomething',
-    db: 'codeial_development',
+    db: 'social_development',
     smtp: {
         service: 'gmail',
         host: 'smtp.gmail.com',
@@ -19,7 +19,24 @@ const development = {
     jwt_secret: 'social'
 }
 const production = {
-    name: 'production'
+    name: 'production',
+    asset_path: process.env.SOCIAL_ASSET_PATH,
+    session_cookie_key: process.env.SOCIAL_SESSION_COOKIE_KEY,
+    db: process.env.SOCIAL_DB,
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.SOCIAL_GMAIL_USERNAME,
+            pass: process.env.SOCIAL_GMAIL_PASSWORD
+        }
+    },
+    google_client_id: process.env.SOCIAL_GOOGLE_CLIENT_ID,
+    google_client_secret: process.env.SOCIAL_GOOGLE_CLIENT_SECRET,
+    google_call_back_url: process.env.SOCIAL_GOOGLE_CALL_BACK_URL,
+    jwt_secret: process.env.SOCIAL_JWT_SECRET
 }
 
-module.exports = development;
+module.exports = eval(process.env.SOCIAL_ENVIRONMENT) == undefined ? development : eval(process.env.SOCIAL_ENVIRONMENT);
